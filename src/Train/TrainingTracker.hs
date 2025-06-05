017522d35208163b10e248f1e5d76202bc0a0eb7
Theo <thoecastillo@yahoo.com>
2025-06-05 17:51:44 +0900

    Refactor TrainingTracker to include detailed metrics and add unit tests for save/load functionality

--- src/Train/TrainingTracker.hs
+++ src/Train/TrainingTracker.hs
@@ -1,16 +1,55 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Train.TrainingTracker where
 import Config
+import GHC.Generics         (Generic)
+import Data.Aeson          (ToJSON, FromJSON, encode, decode)
+import qualified Data.ByteString.Lazy as BL
 
 data TrainingTracker = TrainingTracker
   { currentEpoch :: Int
   , currentBatch :: Int
-  , currentLr :: Double
-  , loss :: [Double]
-  , accuracy :: [Double]
+  , trainLoss :: [Float]
+  , validLoss :: [Float]
+  , trainAccuracy :: [Float]
+  , validAccuracy :: [Float]
   , lastModelPath :: String
-  } deriving (Show, Eq)
+  } deriving (Eq,Generic)
 
 
+initialTrainingTracker :: TrainingTracker
+initialTrainingTracker = TrainingTracker
+  { currentEpoch = 0
+  , currentBatch = 0
+  , trainLoss = []
+  , validLoss = []
+  , trainAccuracy = []
+  , validAccuracy = []
+  , lastModelPath = ""
+  }
+
+instance Show TrainingTracker where
+  show tracker =
+    "TrainingTracker { "
+    ++ "currentEpoch: " ++ show (currentEpoch tracker) ++ ", "
+    ++ "currentBatch: " ++ show (currentBatch tracker) ++ ", "
+    ++ "trainLoss: " ++ show (trainLoss tracker) ++ ", "
+    ++ "validLoss: " ++ show (validLoss tracker) ++ ", "
+    ++ "trainAccuracy: " ++ show (trainAccuracy tracker) ++ ", "
+    ++ "validAccuracy: " ++ show (validAccuracy tracker) ++ ", "
+    ++ "lastModelPath: " ++ lastModelPath tracker
+    ++ " }"
+
+instance ToJSON TrainingTracker
+instance FromJSON TrainingTracker
+
+saveTrainingTracker :: FilePath -> TrainingTracker -> IO ()
+saveTrainingTracker path tracker =
+  BL.writeFile path (encode tracker)
+
+loadTrainingTracker :: FilePath -> IO (Maybe TrainingTracker)
+loadTrainingTracker path = do
+  content <- BL.readFile path
+  return (decode content)
 
 
 
---
+++ test/TrainingTrackerTest.hs
@@ -0,0 +1,42 @@
+module TrainingTrackerTest where
+
+import Train.TrainingTracker
+import System.Directory (removeFile)
+
+
+
+import Test.Hspec
+
+
+
+trainingTrackerSaveAndLoad :: Spec
+trainingTrackerSaveAndLoad = do
+    let tt = TrainingTracker { 
+        currentEpoch = 10
+    , currentBatch = 1024
+    , trainLoss = [10.0, 9.5, 8.7]
+    , validLoss = [10.0, 9.8, 9.2]
+    , trainAccuracy = []
+    , validAccuracy = [0.8, 0.85, 0.9]
+    , lastModelPath = "Hello"
+  }
+    let filePath = "training_tracker_test.json"
+        
+    it "TrainingTracker must be save and load" $ do
+        saveTrainingTracker filePath tt
+        loadedTT <- loadTrainingTracker filePath
+        removeFile filePath
+        case loadedTT of
+            Just loaded -> do
+                currentEpoch loaded `shouldBe` currentEpoch tt
+                currentBatch loaded `shouldBe` currentBatch tt
+                trainLoss loaded `shouldBe` trainLoss tt
+                validLoss loaded `shouldBe` validLoss tt
+                trainAccuracy loaded `shouldBe` trainAccuracy tt
+                validAccuracy loaded `shouldBe` validAccuracy tt
+                lastModelPath loaded `shouldBe` lastModelPath tt
+                tt `shouldBe` loaded
+            Nothing -> expectationFailure "Failed to load TrainingTracker"
+
+        
+
